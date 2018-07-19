d5825cc7d40f704716ed31b781e08922c5adc741
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2018-07-19 16:45:44 +0200

    Newer version with uglify and concat

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,21 @@ var print = require('gulp-print').default;
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var inject = require("gulp-inject");
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 // TODO: var sassdoc = require('sassdoc');
+// TODO: add flag to add sourcemaps only for DEV environment
 var fs = require('fs');
 
-var dir = './public/assets/';
+var dir = './public_html/assets/';
 var dir_css = dir + 'css/';
 var dir_sass = dir + 'sass/';
 var dir_js = dir + 'js/';
+var dir_js_src = dir_js + 'src/';
+var dir_js_compressed = dir_js_src + 'compressed/';
+var dir_js_dist = dir_js + 'dist/';
+var dir_js_dist1 = './js/';
 var dir_maps = dir + 'maps/';
 var dir_html = './public/';
 
@@ -40,12 +48,6 @@ gulp.task('sasslint', function () {
   return gulp.src(dir_sass + '**/*.s+(a|c)ss')
           .pipe(print())
           .pipe(sassLint({
-            /*rules: {
-             'class-name-format': {
-             'allow-leading-underscore': false,
-             'convention': 'camelcase'
-             }
-             },*/
             configFile: './.sass-lint.yml'
           }))
           .pipe(sassLint.format())
@@ -55,7 +57,7 @@ gulp.task('sasslint', function () {
 gulp.task('styles', () =>
   gulp.src(dir_css + 'src/**/*.css')
           .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ['cover 99.5%'],
             cascade: false
           }))
 );
@@ -63,7 +65,7 @@ gulp.task('styles', () =>
 gulp.task('lint', function () {
   const hasFixFlag = (process.argv.slice(2).indexOf('--fix') >= 0);
 
-  return gulp.src(dir_js + '/src/*.js')
+  return gulp.src(dir_js_src + '/*.js')
           .pipe(print())
           .pipe(eslint({
             fix: hasFixFlag
@@ -106,6 +108,27 @@ gulp.task('sass_critical', ['sasslint'], function () {
           .pipe(gulp.dest(dir_css));
 });
 
+// Minifies JS
+gulp.task('uglify', function(){
+    return gulp.src(dir_js_src + '*.js')
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(dir_js_compressed));
+});
+
+gulp.task('odd_file', function(){
+    return gulp.src([dir_js_compressed + 'set-background.js'])
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(dir_js_dist));
+});
+
+gulp.task('js_concat', ['uglify'], function(){
+    return gulp.src([dir_js_compressed + '*.js', '!'+ dir_js_compressed + 'set-background.js'])
+    .pipe(concat('bundle.min.js'))
+    .pipe(gulp.dest(dir_js_dist1))
+    .pipe(gulp.dest(dir_js_dist));
+});
+
 gulp.task('sass:watch', function () {
   gulp.watch(dir_sass + '**/*.s+(a|c)ss', ['sass']);
 });
@@ -123,5 +146,5 @@ gulp.task('inject', ['sass_critical'], function () {
           .pipe(gulp.dest(dir_html));
 });
 
-gulp.task('default', ['sass', 'inject', 'lint'], function () {
-});
+gulp.task('default', ['sass', 'inject', 'lint'], function () {});
+gulp.task('prod', ['default', 'js_concat', 'odd_file'], function () {});
